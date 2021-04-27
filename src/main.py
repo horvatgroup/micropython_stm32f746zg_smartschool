a77cdc3c9c8911f7eca16a301b0904d8aaa6bedc
schef <zvanstefan@gmail.com>
2021-04-27 08:54:53 +0200

    working subscribe

--- src/main.py
+++ src/main.py
@@ -2,13 +2,15 @@ from common import dump_func, create_led, create_button
 from peripherals import register_button_callback_function, check_button
 import network
 from umqtt_simple import MQTTClient
-import ubinascii, machine
+import ubinascii
+import machine
 
 SERVER = "192.168.88.75"
 CLIENT_ID = ubinascii.hexlify(machine.unique_id())
 TOPIC = b"led"
 lan = None
 client = None
+
 
 @dump_func(timing=True)
 def test_network():
@@ -39,25 +41,32 @@ def http_get(url):
             break
     s.close()
 
+
+def sub_cb(topic, msg):
+    print((topic, msg))
+
+
 @dump_func(timing=True)
 def test_mqtt():
     global client
     client = MQTTClient(CLIENT_ID, SERVER)
     client.connect()
+    client.set_callback(sub_cb)
+    client.subscribe(b"foo_topic")
     #TODO: handle
-    #OSError: [Errno 104] ECONNRESET
-    print("Connected to %s, waiting for button presses" % SERVER)
-    client.publish(TOPIC, b"toggle")
-    client.disconnect()
+    # OSError: [Errno 104] ECONNRESET
+    # client.disconnect()
+
 
 def on_button_callback(state):
-    if state:
-        http_get('http://micropython.org/ks/test.html')
-        test_mqtt()
+    client.publish(TOPIC, "toggle %s".encode() % (state))
 
 
 if __name__ == "__main__":
     test_network()
+    http_get('http://micropython.org/ks/test.html')
+    test_mqtt()
     register_button_callback_function(on_button_callback)
     while True:
         check_button()
+        client.check_msg()
