0988846807cbed7da31ff99522b41e5ea3a99e93
schef <zvanstefan@gmail.com>
2024-04-26 14:44:16 +0200

    fixed timestamp and lof to file

--- extra/rpi_monitor/tmux.sh
+++ extra/rpi_monitor/tmux.sh
@@ -9,13 +9,9 @@ else
     tmux split-pane -t $SESSION:0.0
     tmux select-layout even-vertical
     tmux select-layout -t $SESSION even-vertical
-    tmux send-keys -t rpi_monitor:0.0 
-    tmux send-keys -t $SESSION:0.0 "~/.config/tmux/plugins/tmux-logging/scripts/toggle_logging.sh" Enter
-    tmux send-keys -t $SESSION:0.1 "~/.config/tmux/plugins/tmux-logging/scripts/toggle_logging.sh" Enter
-    tmux send-keys -t $SESSION:0.2 "~/.config/tmux/plugins/tmux-logging/scripts/toggle_logging.sh" Enter
     tmux send-keys -t $SESSION:0.0 "cd ~/rpi_monitor/" Enter
-    tmux send-keys -t $SESSION:0.1 "tio /dev/ttyAMA0" Enter
-    tmux send-keys -t $SESSION:0.2 "tio /dev/ttyACM0" Enter
+    tmux send-keys -t $SESSION:0.1 "tio /dev/ttyAMA0 -l -t" Enter
+    tmux send-keys -t $SESSION:0.2 "tio /dev/ttyACM0 -l -t" Enter
     tmux select-pane -t $SESSION:0.0
     tmux attach-session -t "$SESSION"
 fi
